4cb3546e9df37ebf923a9ab1e33cbded8b1cfd79
textEditUser <runningzoomcat@gmail.com>
2021-04-28 00:59:17 +1000

    ah it fukcing worked thsi time lord i had a small heart attack

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+// serialisation to db is ok, but reconstructing the array and processing it is just terrible
+
 const fs = require("fs");
 const http = require("http");
 const https = require("https");
 const express = require("express");
+const redis = require("redis");
+const fetch = require("node-fetch");
 
 httpsOptions = {
 	cert: fs.readFileSync("./ssl/back_textedit_dev.crt"),
@@ -9,8 +13,9 @@ httpsOptions = {
 	key: fs.readFileSync("./ssl/back_textedit_dev.key")
 }
 
+// express app config
 const app = express();
-
+app.use(express.static("./public"));
 app.use((req, res, next) => {
 	if(req.protocol === "http") {
 		res.redirect(301, `https://${req.headers.host}${req.url}`)
@@ -18,12 +23,97 @@ app.use((req, res, next) => {
 	next()
 })
 
-app.use(express.static("./public"));
+// redis app config
+const rclient = redis.createClient("redis://redis:6379");
+rclient.on("error", function(error) {
+  console.error(error);
+});
 
+// end points
+// im too cool for error handling. if the api responds with unexpected data, LET THE APP CRASH
+
+// core api end point for front end
 app.get("/API", (req, res) => {
-	res.json("awesome")
+	rclient.KEYS("WEATHER:TEMP:HISTORY:*", (err, reply) => { // returns array of keys
+		keyList = reply
+		// awful bubble sort copied from other code
+		let cyclePass = [false] 
+		while(cyclePass.includes(false) == true) {
+			cyclePass = []
+			for(i in keyList) {
+				try {
+					if(parseInt(keyList[i].split(":")[3]) > parseInt(keyList[parseInt(i) + 1].split(":")[3])) {
+						[keyList[i], keyList[parseInt(i) + 1]] = [keyList[parseInt(i) + 1], keyList[i]];
+						cyclePass.push(false)
+					}
+					else {
+						cyclePass.push(true)
+					}
+				}catch(e){}
+			}
+		}
+		let greatArray = [] // how to return x inside of callback to map arrow function???
+		reply.map(x => rclient.GET(x, (y, x) => greatArray.push(x)))
+		setTimeout(() => res.json(greatArray), 1000)
+	})
 })
 
+function historyfetch(callback) {
+	// I was forced to spoof the user agent, because BOM dont like web scrapers on json endpoints??? 
+	// told me "eufuhef automated request detected go away" so i was forced to spoof it and hope i dont end up with my ip blacklisted or something
+	fetch("http://www.bom.gov.au/fwo/IDV60701/IDV60701.95936.json", {
+	  "headers": {
+	    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+	    "accept-language": "en-US,en;q=0.9",
+	    "cache-control": "no-cache",
+	    "pragma": "no-cache",
+	    "upgrade-insecure-requests": "1",
+	    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36 OPR/75.0.3969.218",
+	    "cookie": process.env.cookie
+	  },
+	  "referrerPolicy": "strict-origin-when-cross-origin",
+	  "body": null,
+	  "method": "GET",
+	  "mode": "cors"
+	}).then(res => { return res.json() }) // parse data // res.text().then(x => console.log(x)); 
+	.then(data => {
+		// data object generation struc
+		// newdata; head of the object
+		// temp; object; temperature data
+		// overtime; an array of objects; {temp(int), date(string)}; air temp polled every 30 minutes
+		// ... date(string) is formatted year/month/day/hour/minute/second with each being two digets except year
+		// ... example being 20210426213000
+		let newData = {
+			"temp": {
+				"overtime": [] // {temp, date}
+			}
+		}
+		for(i in data.observations.data) {
+			newData.temp.overtime.push({ "temp": data.observations.data[i].air_temp, "date": data.observations.data[i].local_date_time_full })
+		}
+		// redis db struc
+		// db_name:element:type
+		// type history uses :date with the data being the temp
+		for(i in newData.temp.overtime) {
+			rclient.set(`WEATHER:TEMP:HISTORY:${newData.temp.overtime[i].date}`, `${newData.temp.overtime[i].temp}`)
+		}
+		callback()
+	})
+}
+
+setInterval(historyfetch, 1800000) // every 30 minutes
+
+historyfetch(() => {
+	setTimeout(() => {
+		rclient.KEYS("WEATHER:TEMP:HISTORY:*", (err, reply) => { // returns array of keys
+			reply.map(x => rclient.GET(x, (y, x) => console.log(x)))
+			// map is NOT to be used like this. it is for generating
+			// new arrays. but uh ... operateor wouldnt work.
+		})
+	}, 2000)
+})
+
+// status server polling
 setInterval(() => {
 	fetch("http://interstatus:50001/update", {
 		method: "post",
@@ -32,5 +122,8 @@ setInterval(() => {
 	}).catch(e => { console.log("status server down") })
 }, 20000)
 
+// server initialisation
 const httpsServer = https.createServer(httpsOptions, app)
-httpsServer.listen(50250, "0.0.0.0", () => console.log("listening 433"))
+httpsServer.listen(50300, "0.0.0.0", () => console.log("listening 433"))
+// const httpServer = http.createServer(app)
+// httpServer.listen(3000, () => console.log("listening"))
